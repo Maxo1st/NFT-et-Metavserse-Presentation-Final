0d2ccb4374927b6728bcdc6e94ec6edbbfc3af94
Maxo1st <84914521+Maxo1st@users.noreply.github.com>
2025-02-17 14:13:57 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import GridSquares from "../components/grid-squares"
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-white">
       <Head>
         <title>Passeport Digital</title>
         <meta name="description" content="Passeport Digital pour les étudiants de Paris Dauphine" />
@@ -13,14 +13,14 @@ export default function Home() {
       </Head>
 
       <Header />
-      <main>
-        <div className="pt-2">
+      <div className="flex min-h-[calc(100vh-140px)]">
+        <aside className="w-64 border-r">
           <NavSquares />
-        </div>
-        <div className="p-4 max-w-6xl mx-auto">
+        </aside>
+        <main className="flex-1 p-6">
           <GridSquares />
-        </div>
-      </main>
+        </main>
+      </div>
     </div>
   )
 }
