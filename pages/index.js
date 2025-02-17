1f8819f37cbe83bdb4afb271419783cc99cce97b
Maxo1st <84914521+Maxo1st@users.noreply.github.com>
2025-02-17 10:26:07 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>NFT et Metaverse Presentation</title>
+        <meta name="description" content="NFT et Metaverse Presentation" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Welcome to NFT et Metaverse Presentation</h1>
+        <p>This is the main page of our application.</p>
+      </main>
+    </div>
+  )
+}
+
