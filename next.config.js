da58a1595f6dc5c49298a6734ca1432b8ce9b565
Maxo1st <84914521+Maxo1st@users.noreply.github.com>
2025-02-17 10:30:20 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/NFT-et-Metavserse-Presentation-Final",
+  assetPrefix: "/NFT-et-Metavserse-Presentation-Final",
+}
+
+module.exports = nextConfig
+
